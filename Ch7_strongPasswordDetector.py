0ce248b555ff24617e7bc4b307f359ea8e8f3b2f
Evan <thegeekforhire@gmail.com>
2019-04-16 19:20:27 -0500

    Finished Ch7_strongPasswordDetector.py, all tests pass

--- Ch7_strongPasswordDetector.py
+++ Ch7_strongPasswordDetector.py
@@ -9,47 +9,65 @@ import re
 
 def validateStrongPass(password):
 
-	minimumLength = re.compile(r'.{8,}')
-	#TODO: Check if string is long enough, 8 at minimum
-	containsUpperCase = re.compile(r'[A-Z]')
-	#TODO: Contains uppercase
-	containsLowerCase = re.compile(r'[a-z]')
-	#TODO: Contains lowercase
-	containsNumber = re.compile(r'[0-9]')
-	#TODO: Contains at least one number
+	minimumLength = re.compile(r'.{8,}') # Check if string is long enough, 8 at minimum
+	containsUpperCase = re.compile(r'[A-Z]') # Contains uppercase
+	containsLowerCase = re.compile(r'[a-z]') # Contains lowercase
+	containsNumber = re.compile(r'[0-9]') # Contains at least one number
 	
 	strong = True
 	
 	matchObject = minimumLength.search(password)
 	if matchObject == None:
-		print('Bad')
+		print('Password should be at least 8 characters in length')
 		strong = False
+	
+	matchObject = containsUpperCase.search(password)
+	if matchObject == None:
+		print('Password should contain at least 1 uppercase character')
+		strong = False
+	
+	matchObject = containsLowerCase.search(password)
+	if matchObject == None:
+		print('Password should contain at least 1 lowercase character')
+		strong = False
+		
+	matchObject = containsNumber.search(password)
+	if matchObject == None:
+		print('Password should contain at least 1 number')
+		strong = False
+		
+	if strong:
+		print('Entered password is strong!')
 		
 	return strong
 	
-	#TODO: Logic to test each case
-	#TODO: 
-	#TODO: 
-	pass
-
+	
 class Test_validateStrongPass():
 	@pytest.mark.parametrize("input, output",[('hello',"Password should be at least 8 characters in length\nPassword should contain at least 1 uppercase character\nPassword should contain at least 1 number\n"), 
-	('WE','Password should be longer than 7 characters\nPassword should contain at least 1 lowercase character\nPassword should contain at least 1 number\n'),
+	('WE','Password should be at least 8 characters in length\nPassword should contain at least 1 lowercase character\nPassword should contain at least 1 number\n'),
 	('TH1s',"Password should be at least 8 characters in length\n"),
 	('thisd0esnt','Password should contain at least 1 uppercase character\n'),
 	('NOTTHIS1','Password should contain at least 1 lowercase character\n'),
 	('NopeForThis','Password should contain at least 1 number\n')])
-	def test_failingPasswords(self, monkeypatch, capsys, input, output):
+	def test_failingPasswords(self, capsys, input, output):
 		logging.debug('Start of test_failingPasswords, input, output: %s, %s' % (str(input), str(output)))
 		
 		result = validateStrongPass(input)
+		stdout_capture = capsys.readouterr() # Get captured stdout
 		
-		stdout_capture = capsys.readouterr()
 		assert stdout_capture[0] == output
 		assert result == False
 		
-	def test_passingPasswords(self):
-		pass
+	@pytest.mark.parametrize("input, output",[('Th1sWorks',"Entered password is strong!\n"), 
+	('C4ntBr34kmeD0wn','Entered password is strong!\n')])
+	def test_passingPasswords(self, capsys, input, output):
+		logging.debug('Start of test_passingPasswords, input, output: %s, %s' % (str(input), str(output)))
+		
+		result = validateStrongPass(input)
+		stdout_capture = capsys.readouterr() # Get captured stdout
+		
+		assert stdout_capture[0] == output
+		assert result == True
 	pass
 	
 if __name__ == '__main__':
