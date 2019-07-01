ed3029094d66cd69ece0b7e68fe4cccfbe889aa1
Evan <thegeekforhire@gmail.com>
2019-07-01 10:21:03 -0500

    Okay, for REAL this time. Deleted old bookProjects folder

--- bookProjects/Ch8_regexSearch.py
+++
@@ -1,5 +0,0 @@
-#Regex Search
-#Chapter 8 of Automate the Boring Stuff with Python
-'''
-Write a program that opens all .txt files in a folder and searches for any line that matches a user-supplied regular expression. The results should be printed to the screen.
-'''
