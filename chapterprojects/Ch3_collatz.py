ac9181032eeb3706ba8d74e954c37b8dbd264ee6
Evan <thegeekforhire@gmail.com>
2019-07-02 10:37:00 -0500

    Trying to get entry-points for the project working, currently broken

--- chapterprojects/Ch3_collatz.py
+++ chapterprojects/Ch3_collatz.py
@@ -1,10 +1,19 @@
 #The Collatz Sequence
 #Chapter 3 of Automate the Boring Stuff with Python
 '''
-Write a function named collatz() that has one parameter named number. If number is even, then collatz() should print number // 2 and return this value. If number is odd, then collatz() should print and return 3 * number + 1.
-Then write a program that lets the user type in an integer and that keeps calling collatz() on that number until the function returns the value 1. (Amazingly enough, this sequence actually works for any integer—sooner or later, using this sequence, you’ll arrive at 1! Even mathematicians aren’t sure why. Your program is exploring what’s called the Collatz sequence, sometimes called “the simplest impossible math problem.”)
-Remember to convert the return value from input() to an integer with the int() function; otherwise, it will be a string value.
-Hint: An integer number is even if number % 2 == 0, and it’s odd if number % 2 == 1.
+Write a function named collatz() that has one parameter named number. If number
+is even, then collatz() should print number // 2 and return this value. 
+If number is odd, then collatz() should print and return 3 * number + 1.
+Then write a program that lets the user type in an integer and that keeps 
+calling collatz() on that number until the function returns the value 1. 
+(Amazingly enough, this sequence actually works for any integer—sooner or 
+later, using this sequence, you’ll arrive at 1! Even mathematicians aren’t 
+sure why. Your program is exploring what’s called the Collatz sequence, 
+sometimes called “the simplest impossible math problem.”)
+Remember to convert the return value from input() to an integer with the int() 
+function; otherwise, it will be a string value.
+Hint: An integer number is even if number % 2 == 0, and it’s odd if 
+number % 2 == 1.
 The output of this program could look something like this:
 Enter number:
 3
@@ -20,6 +29,7 @@ Enter number:
 import logging
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 logging.disable(logging.DEBUG)
+
 
 def collatz(number):
     logging.debug('Start of collatz(%s)' % (number))
@@ -46,8 +56,9 @@ def collatzLoop(number):
     logging.debug('collatzLoop is returing: %s' % number)
     return number
 
-if __name__ == '__main__':
+def main():
 	logging.debug('Start of program')
+	print("The Collatz Sequence - Ch3_collatz.py\n")
 	number = input("Enter a number: ")
 	try:
 		number = int(number)
@@ -56,3 +67,6 @@ if __name__ == '__main__':
 	else:
 		collatzLoop(number)
 		logging.debug('End of program')
+
+if __name__ == '__main__':
+	main()
---
+++ chapterprojects/__main__.py
@@ -0,0 +1,13 @@
+import sys
+from Sweigart-Book-Projects import Ch3_collatz
+
+
+def main(args=None):
+    if args is None:
+        args = sys.argv[1:]
+
+    print("This is the main routine.")
+
+
+if __name__ == "__main__":
+    main()
